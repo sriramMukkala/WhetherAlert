07780d6cf04f546f7de26b16fb778cf0e76059ab
Larsen.Raja <Larsen.Raja@IN2203691W1.ey.net>
2017-10-03 12:01:15 +0530

    Base class for policy holder operations

--- src/main/java/com/ey/insurancealerts/services/PolicyHolderServices.java
+++ src/main/java/com/ey/insurancealerts/services/PolicyHolderServices.java
@@ -4,12 +4,23 @@ import com.ey.insurancealerts.dao.GenericDAO;
 import com.ey.insurancealerts.dao.GenericDAOImpl;
 import com.ey.insurancealerts.models.BaseEntity;
 
-public class UserServices {
+public class PolicyHolderServices {
 
-	
 	GenericDAO<BaseEntity, String> dao = new GenericDAOImpl();
 	
-	public void addUsers() {
+	public void updatePolicyHolderLocation() {
+		
+	}
+	
+	public void updatePolicyHolderContactInfo() {
+		
+	}
+	
+	public void deletePolicyHolder() {
+		
+	}
+	
+	public void addPolicyHolder() {
 		
 	}
 	

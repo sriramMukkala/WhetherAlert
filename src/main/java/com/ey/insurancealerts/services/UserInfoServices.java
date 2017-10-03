a725782eca46b5f1183a7b40e8cc556bcc95f6e9
Larsen.Raja <Larsen.Raja@IN2203691W1.ey.net>
2017-10-03 11:53:43 +0530

    Fixed compilation errors

--- src/main/java/com/ey/insurancealerts/services/UserInfoServices.java
+++ src/main/java/com/ey/insurancealerts/services/UserInfoServices.java
@@ -1,9 +1,33 @@
 package com.ey.insurancealerts.services;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.Part;
+
+import com.ey.insurancealerts.dao.GenericDAO;
+import com.ey.insurancealerts.dao.GenericDAOImpl;
+import com.ey.insurancealerts.models.UserInfo;
+
 public class UserInfoServices {
 
-	public Response addUser() {
-		
+	GenericDAO dao = new GenericDAOImpl();
+
+	public void addUser(Part part) throws IOException {
+		InputStream inputStream = part.getInputStream();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+		String line;
+		StringBuffer sb = new StringBuffer();
+		while ((line = reader.readLine()) != null) {
+			sb.append(line);
+		}
+
+		List<UserInfo> userInfoList = new ArrayList<>();
+		userInfoList.forEach(dao::create);
 	}
-	
+
 }
